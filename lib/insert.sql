869c685c192359e361dbf30e4dc703056c2d8b64
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-08-27 18:20:28 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex) VALUES
+  ('Mr. Chocolate', 20, 'M'),
+  ('Rowdy', 10, 'M'),
+  ('Tabitha', 6, 'F'),
+  ('Sergeant Brown', 19, 'M'),
+  ('Melissa', 13, 'F'),
+  ('Grinch', 2, 'M'),
+  ('Wendy', 6, 'F'),
+  (NULL, 20, 'M');
